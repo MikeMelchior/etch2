ad21f451e713df8f7eae94f58888d5139b80769d
Mike Melchior <mike.melchior@outlook.com>
2023-01-13 16:30:35 -0500

    add grid creation logic with slider functionality

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 // main div contains all content on page
 
 const mainDiv = document.querySelector('.main');
-const selectionPopUpWindow = document.querySelector('.size-selection-window');
 
+// create container for bulk of content to control pointer events when pop up windows are open
+
+const mainContent = document.createElement('div');
+mainContent.classList.add('main-content');
+mainDiv.appendChild(mainContent)
 
 // create H1 title for page
 
 const h1 = document.createElement('h1');
 h1.textContent = 'etch-a-sketch';
-mainDiv.appendChild(h1);
+mainContent.appendChild(h1);
 
 
 // create button to open window for selecting a grid size
@@ -16,42 +20,101 @@ mainDiv.appendChild(h1);
 const sizeButton = document.createElement('button')
 sizeButton.classList.add('size-button');
 sizeButton.textContent = "Select a grid size"
-mainDiv.appendChild(sizeButton);
+mainContent.appendChild(sizeButton);
 
 
 // create pop up window for grid size selection slider
-    //create container
 
-const sizeSelectionDiv = document.createElement('div');
+        //create container
 
-    //add class to pop up window for styling
+    const sizeSelectionDiv = document.createElement('div');
 
-sizeSelectionDiv.classList.add('size-selection-window');
+        //add class to pop up window for styling
 
-    //create label and slider for size selection
-const sizeLabel = document.createElement('label');
-sizeLabel.htmlFor = 'Grid Size';
-sizeLabel.textContent = 'Grid size (between 10 and 200)';
+    sizeSelectionDiv.classList.add('size-selection-window','hidden','un-clickable');
 
-const sizeSlider = document.createElement('input');
-sizeSlider.type = 'range';
-sizeSlider.id = 'size'
-sizeSlider.name = 'size';
-sizeSlider.min = 10;
-sizeSlider.max = 200
+        //create label for grid size selection
+
+    const sizeLabel = document.createElement('label');
+    sizeLabel.htmlFor = 'Grid Size';
+    sizeLabel.textContent = 'please make a selection between 10 and 100 that will determine the height and width of your grid';
+
+        // create container for slider and slider value display
+
+    const sliderContainer = document.createElement('div');
+    sliderContainer.classList.add('slider-container');
+
+        // create slider for grid size selection
+
+    const sizeSlider = document.createElement('input');
+    sizeSlider.type = 'range';
+    sizeSlider.id = 'size'
+    sizeSlider.name = 'size';
+    sizeSlider.min = 10;
+    sizeSlider.max = 100
+
+        // create slider value display
+
+    const sliderValue = document.createElement('p');
+    sliderValue.classList.add('slider-value');
+    sliderValue.textContent = '50';
+
+        // append slider and slider value display to container
+
+    sliderContainer.appendChild(sizeSlider);
+    sliderContainer.appendChild(sliderValue);
+
+        // create container for buttons
+
+    const popupButtonsContainer = document.createElement('div');
+    popupButtonsContainer.classList.add('popup-buttons-container');
+
+            // create cancel button to exit popup window without changes
+
+        const cancelSizeSelectionButton = document.createElement('button');
+        cancelSizeSelectionButton.textContent = '~   CANCEL   ~';
+        cancelSizeSelectionButton.classList.add('cancel-button');
+
+            // create confirmation button to apply changes
+        
+        const startButton = document.createElement('button');
+        startButton.textContent = 'Start Drawing!';
+        startButton.classList.add('start-button');
+
+            // append buttons to container
+        
+        popupButtonsContainer.appendChild(cancelSizeSelectionButton);
+        popupButtonsContainer.appendChild(startButton);
 
 
-    //append slider to pop up window and append window to main div
+    //append content to pop up window and append window to main div
+
 sizeSelectionDiv.appendChild(sizeLabel);
-sizeSelectionDiv.appendChild(sizeSlider);
+sizeSelectionDiv.appendChild(sliderContainer);
+sizeSelectionDiv.appendChild(popupButtonsContainer);
 mainDiv.appendChild(sizeSelectionDiv)
 
+
+
+//                                                  //
+//                                                  //
+// ~~~~~~~~~~~~~ CREATING GRID ~~~~~~~~~~~~~~~~~~~~ //
+//                                                  //
+//                                                  //
 
     // create container for grid
 
 const etchContainer = document.createElement('div');
 etchContainer.classList.add('etch-container');
-mainDiv.appendChild(etchContainer)
+mainContent.appendChild(etchContainer)
+
+    // create aesthetic knobs for bottom of 'etch-a-sketch'
+
+const knobsContainer = document.createElement('div');
+knobsContainer.classList.add('knobs-container');
+mainContent.appendChild(knobsContainer);
+
+    
 
 
     // function for filling grid container with appropriate amount of pixels
@@ -59,6 +122,10 @@ mainDiv.appendChild(etchContainer)
 const createSketchBoard  = (width) => {
     const etchContainer = document.querySelector('.etch-container');
 
+    mainContent.removeChild(etchContainer);
+    const newContainer = document.createElement('div');
+    newContainer.classList.add('etch-container');
+    
     for (let i = 0; i < width; i++) {
         let div = document.createElement('div');
         div.classList.add(`row${i}`,'grid-row');
@@ -67,31 +134,69 @@ const createSketchBoard  = (width) => {
             pixel.classList.add(`column${j}`,'grid-pixel');
             div.appendChild(pixel);
         }
-        etchContainer.appendChild(div);
+        newContainer.appendChild(div);
     }
-    return mainDiv.appendChild(etchContainer);
+    return mainContent.insertBefore(newContainer, knobsContainer);
 }
 
-const hideWindow = () => {
-    return classList.add('hidden');
+
+
+
+
+
+
+
+
+
+
+const selectionPopUpWindow = document.querySelector('.size-selection-window');
+
+
+
+const hideWindow = (window) => {
+    return window.classList.add('hidden');
 }
 
-const showWindow = () => {
-    return classList.add('visible');
+const showWindow = (window) => {
+    return window.classList.add('visible');
 }
 
-const showSizeSelection = () => {
+const Clickable = (window) => {
+    return window.classList.toggle('un-clickable');
+}
+
+const showSizeSelectionWindow = () => {
     selectionPopUpWindow.classList.remove('hidden');
-    selectionPopUpWindow.showWindow();
+    showWindow(selectionPopUpWindow);
+    Clickable(selectionPopUpWindow);
+    Clickable(mainContent)
 }
 
-const hideSizeSelection = () => {
+const hideSizeSelectionWindow = () => {
     selectionPopUpWindow.classList.remove('visible');
-    selectionPopUpWindow.hideWindow();
+    hideWindow(selectionPopUpWindow);
+    Clickable(selectionPopUpWindow);
+    Clickable(mainContent);
+}
+
+const updateSliderValue = () => {
+    let slider = document.querySelector('#size');
+    let display = document.querySelector('.slider-value');
+    display.textContent = slider.value
+}
+
+const startGrid = (value) => {
+    value = document.querySelector('#size').value;
+    return createSketchBoard(value);
 }
 
 
-sizeButton.addEventListener('click', showSizeSelection)
+sizeButton.addEventListener('click', showSizeSelectionWindow)
+document.querySelector('#size').addEventListener('click', updateSliderValue)
+
+const startGridButton = document.querySelector('.start-button');
+startGridButton.addEventListener('click', startGrid);
+startGridButton.addEventListener('click', hideSizeSelectionWindow)
 
 
 
@@ -100,12 +205,8 @@ sizeButton.addEventListener('click', showSizeSelection)
 
 
 
-let slider = document.querySelector('#size');
 
 
-const testFunc = (e) => {
-    console.log(size.value);
-};
 
 
-size.addEventListener('click', testFunc)
+// size.addEventListener('click', testFunc);
